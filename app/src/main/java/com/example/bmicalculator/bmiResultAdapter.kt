e2b7ac2c37726c0079c24cea132824dd3e0f3bfc
59160927 <59160927@go.buu.ac.th>
2019-11-09 01:57:09 -0800

    RecyclerView 2

--- app/src/main/java/com/example/bmicalculator/bmiResultAdapter.kt
+++ app/src/main/java/com/example/bmicalculator/bmiResultAdapter.kt
@@ -1,29 +1,41 @@
 package com.example.bmicalculator
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bmicalculator.database.BMI
 
-class bmiResultAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+class bmiResultAdapter: RecyclerView.Adapter<bmiResultAdapter.ViewHolder>() {
     var data =  listOf<BMI>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val height: TextView = itemView.findViewById(R.id.height_table)
+        val weight: TextView = itemView.findViewById(R.id.weight_table)
+        val bmiCost: TextView = itemView.findViewById(R.id.bmi_cost_table)
+        val bmiCriterion: TextView = itemView.findViewById(R.id.bmi_criterion_table)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_bmi_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_bmi, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.bmiQuality.toString()
+        val res = holder.itemView.context.resources
+        holder.height.text = item.height.toString()
+        holder.weight.text = item.weight.toString()
+        holder.bmiCost.text = String.format("%.2f", item.bmiCost)
+        holder.bmiCriterion.text = item.bmiCriterion
     }
 
 }
